2323d21eaeade58efc1bff73ed477e060c636c18
Philipp Piwo <github@piwo.space>
2018-01-18 10:03:46 +0100

    add caching if device is offline

--- app/src/main/kotlin/de/thepiwo/lifelogging/android/dagger/modules/ApplicationModule.kt
+++ app/src/main/kotlin/de/thepiwo/lifelogging/android/dagger/modules/ApplicationModule.kt
@@ -13,12 +13,12 @@ import de.thepiwo.lifelogging.android.dagger.ForApplication
 import de.thepiwo.lifelogging.android.util.*
 import okhttp3.Cache
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 import javax.inject.Named
-import okhttp3.logging.HttpLoggingInterceptor
 
 
 @Module
@@ -81,17 +81,22 @@ class ApplicationModule(private val application: BaseApplication) {
                 .addNetworkInterceptor { chain ->
                     val request = chain.request().newBuilder().addHeader("X-Client", "Android-App").build()
                     chain.proceed(request)
-                }.readTimeout(Constants.HTTP_READ_TIMEOUT, TimeUnit.SECONDS).writeTimeout(Constants.HTTP_WRITE_TIMEOUT, TimeUnit.SECONDS).build()
+                }
+                .readTimeout(Constants.HTTP_READ_TIMEOUT, TimeUnit.SECONDS)
+                .writeTimeout(Constants.HTTP_WRITE_TIMEOUT, TimeUnit.SECONDS)
+                .build()
     }
 
     @Provides
     @Named("authorized")
     @ForApplication
-    fun provideAuthorizedOkHttpClient(cache: Cache, authHelper: AuthHelper): OkHttpClient {
+    fun provideAuthorizedOkHttpClient(cache: Cache, authHelper: AuthHelper, applicationContext: Context): OkHttpClient {
         val interceptor = HttpLoggingInterceptor()
         interceptor.level = HttpLoggingInterceptor.Level.BODY
 
-        return OkHttpClient.Builder().addInterceptor(interceptor).cache(cache)
+        return OkHttpClient.Builder()
+                .addInterceptor(interceptor)
+                .cache(cache)
                 .addNetworkInterceptor { chain ->
                     val request = chain.request().newBuilder().addHeader("Token",
                             if (authHelper.getToken() != null)
@@ -100,7 +105,24 @@ class ApplicationModule(private val application: BaseApplication) {
                                 "").addHeader("X-Client", "Android-App")
                             .build()
                     chain.proceed(request)
-                }.readTimeout(Constants.HTTP_READ_TIMEOUT, TimeUnit.SECONDS).writeTimeout(Constants.HTTP_WRITE_TIMEOUT, TimeUnit.SECONDS).build()
+                }
+                .addNetworkInterceptor { chain ->
+                    val originalResponse = chain.proceed(chain.request())
+                    if (ConnectivityHelper(applicationContext).connected()) {
+                        val maxAge = 60 // read from cache for 1 minute
+                        originalResponse.newBuilder()
+                                .header("Cache-Control", "public, max-age=" + maxAge)
+                                .build()
+                    } else {
+                        val maxStale = 60 * 60 * 24 * 28 // tolerate 4-weeks stale
+                        originalResponse.newBuilder()
+                                .header("Cache-Control", "public, only-if-cached, max-stale=" + maxStale)
+                                .build()
+                    }
+                }
+                .readTimeout(Constants.HTTP_READ_TIMEOUT, TimeUnit.SECONDS)
+                .writeTimeout(Constants.HTTP_WRITE_TIMEOUT, TimeUnit.SECONDS)
+                .build()
     }
 
     @Provides
